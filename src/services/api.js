3b73e3af6f947882a95f14fb64e10c0a167c74e8
pedrogrl <pedro.gos@outlook.com>
2023-01-23 18:56:12 -0300

    .

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,36 @@
 import axios from 'axios';
+import { langColors } from './config';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
 });
+
+export const getUser = async (login) => api.get(`/users/${login}`);
+
+export const getRepos = async (login) => api.get(`/users/${login}/repos`);
+
+export const getDataFrom = (repositories) => {
+  let stats = repositories
+    .map((repo) => repo.language)
+    .reduce(
+      (data, lang) => ({
+        ...data,
+        [lang]: (data[lang] || 0) + 1,
+      }),
+      []
+    );
+
+  delete stats.null;
+
+  stats = Object.keys(stats)
+    .map((lang) => ({
+      name: lang,
+      count: stats[lang],
+      color: langColors[lang.toLowerCase()],
+    }))
+    .sort((a, b) => b.count - a.count);
+
+  return stats;
+};
 
 export default api;
